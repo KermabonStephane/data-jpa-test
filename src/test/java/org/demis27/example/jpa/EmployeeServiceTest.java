6227ebc9f1da452cb4bb6c58f62f4c8576bdf6f4
Kermabon Stéphane <demis27@demis27.net>
2025-02-28 12:07:40 +0100

    Add all tests

--- src/test/java/org/demis27/example/jpa/EmployeeServiceTest.java
+++ src/test/java/org/demis27/example/jpa/EmployeeServiceTest.java
@@ -2,10 +2,12 @@ package org.demis27.example.jpa;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -25,11 +27,32 @@ class EmployeeServiceTest {
     }
 
     @Test
+    @DisplayName("We should have one employee create by the data.sql script")
     void test_init() {
         List<Employee> all = service.getAll();
 
         Assertions.assertThat(all).hasSize(1);
-        Assertions.assertThat(all.getFirst().getFirstname()).isEqualTo("John");
+        Employee johnByrne = all.getFirst();
+        Assertions.assertThat(johnByrne.getFirstname()).isEqualTo("John");
+        Assertions.assertThat(johnByrne.getLastname()).isEqualTo("Byrne");
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO employee (id, firstname, lastname) VALUES (random_uuid(), 'Chris', 'Claremont');")
+    @DisplayName("We should have two employees, one create by the data.sql and one by the statement")
+    void test_statement() {
+        List<Employee> all = service.getAll();
+
+        Assertions.assertThat(all).hasSize(2);
+    }
+
+    @Test
+    @Sql(scripts = "data-test.sql")
+    @DisplayName("We should have three employees, one create by the data.sql and two by the specific script")
+    void test_script() {
+        List<Employee> all = service.getAll();
+
+        Assertions.assertThat(all).hasSize(3);
     }
 
 }
